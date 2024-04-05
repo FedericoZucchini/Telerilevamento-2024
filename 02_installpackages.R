8e5c2d55b95b35cf0e7c3e82caebe6b8d3ed9ac3
Federico Zucchini <162352694+FedericoZucchini@users.noreply.github.com>
2024-04-05 11:31:07 +0200

    Update 02_installpackages.R

--- 02_installpackages.R
+++ 02_installpackages.R
@@ -1,5 +1,38 @@
+APPUNTI 2a lezione (...)
 #Istallazione pacchetti R
 
 install.packages("terra")
 
 library(terra)
+
+
+APPUNTI 2a lezione (...)
+
+# Come installare nuovi pacchetti di R
+# Cominciamo installiando il pacchetto "terra" con il seguente comando
+
+install.packages("terra")
+
+# Proseguiamo installando il pacchetto "devtools" con il seguente comando
+
+install.packages("devtools")
+
+# Andiamo a richiamare nella libreria il pacchetto "terra" appena installato con il comando "library ()"
+
+library(terra)
+
+# Posso in alternativa usare anche il comando "require()"
+# Richiamo in libreria anche devtools
+
+library(devtools)
+
+# Come installare il pacchetto imageRy da GitHub
+
+# Uso una funzione di devtools:
+# Mi occorre usare il comando indicato come segue: install_github("ducciorocchini/imageRy")
+
+install_github("ducciorocchini/imageRy") # Così facendo installo tale pacchetto dal GitHub del prof. Duccio Rocchini 
+
+# Con il comando già visto, "library()" indicando di voler richiamare il pacchetto imageRy, richiamo tale pacchetto
+
+library(imageRy)
