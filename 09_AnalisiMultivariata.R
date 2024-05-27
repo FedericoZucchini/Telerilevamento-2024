1aef1f919bd47ac272b2c27cd4836cb262423049
Federico Zucchini <162352694+FedericoZucchini@users.noreply.github.com>
2024-05-27 17:17:34 +0200

    Update 09_AnalisiMultivariata.R

--- 09_AnalisiMultivariata.R
+++ 09_AnalisiMultivariata.R
@@ -1,8 +1,8 @@
 # Analisi multivariata
 
-# Funzione im.pca () fa tutto quello che serve fare per compattare un set in una sola dimensione
+# Funzione im.pca() fa tutto quello che serve fare per compattare un set in una sola dimensione
 
-# Installare viridis.
+# Installare pacchetto "viridis".
 install.packages("viridis")
 
 # Richiamare le librerie necessarie per lavorare in questo caso
@@ -14,16 +14,16 @@ library(viridis)
 # Lista dei dati
 im.list()
 
-# Importazione dati
+# Importazione dati, immagini relative al blocco delle Tofane in provincia di Belluno
 b2 <- im.import("sentinel.dolomites.b2.tif") # blu
 b3 <- im.import("sentinel.dolomites.b3.tif") # verde
 b4 <- im.import("sentinel.dolomites.b4.tif") # rosso 
 b8 <- im.import("sentinel.dolomites.b8.tif") # nir
 
-# Facciamo una concatenazione con il comanco "c()"
+# Facciamo una concatenazione con il comando "c()"
 sentdo <- c(b2, b3, b4, b8)
 
-# Richiamo sentdo creato poco fa per vedere le proprietà
+# Richiamo "sentdo" creato poco fa per vedere le proprietà
 sentdo
 
 # Visualizzazione dell'immagine
@@ -40,16 +40,7 @@ pairs(sentdo)
 picmage<-im.pca(sentdo)
 
 tot <- sum()
-N*100/tot # Da la variabilità spiegata dal primo asse
-
-# Plot della pca usando viridis
-
-vir <- colorRampPaeltte(viridis(7)) (100)
-plot(pcimage, col= vir)
-plot (pcimage, col= viridis(100))
-plot (pcimage, col=plasma(100)) # Esempio con un'altra legenda di colori
-
-
+N*100/tot #Da la variabilità spiegata dal primo asse
 
 # pca
 pcimage <- im.pca(sentdo)
@@ -59,6 +50,17 @@ tot <- sum(1314.33400, 551.50879, 39.48238, 27.35780)
 
 plot(pcimage, col=viridis(100))
 plot(pcimage, col=plasma(100))
+
+#Plot della pca usando viridis
+
+vir <- colorRampPaeltte(viridis(7)) (100)
+plot(pcimage, col= vir)
+plot (pcimage, col= viridis(100))
+plot (pcimage, col=plasma(100)) # Esempio con un'altra legenda di colori
+
+
+
+
 
 
 
